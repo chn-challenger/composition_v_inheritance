076b75b7af8cf3a93e051902847b557a4e370225
Joe Zhou <chn.challenger@gmail.com>
2016-05-06 20:22:23 +0100

    mountain_bike subclass

--- models/inheritance.rb
+++ models/inheritance.rb
@@ -9,20 +9,27 @@ class RoadBike < Bicycle
   def initialize(args)
     @size = args[:size]
     @tape_colour = args[:tape_colour]
-    @type = args[:type]
+  end
+
+  def spares
+      {chain: '10-speed',tire_size: '23',tape_colour: tape_colour}
+  end
+
+end
+
+class MountainBike < Bicycle
+
+  attr_reader :size, :tape_colour, :type, :front_shock, :rear_shock
+
+  def initialize(args)
+    @size = args[:size]
     @front_shock = args[:front_shock]
     @rear_shock = args[:rear_shock]
   end
 
   def spares
-    # case type
-    # when 'roadbike'
-    #   {chain: '10-speed',tire_size: '23',tape_colour: tape_colour}
-    # when 'mountainbike'
-    #   {chain: '10-speed',tire_size: '2.1',front_shock: front_shock,
-    #     rear_shock: rear_shock}
-    # end
-      {chain: '10-speed',tire_size: '23',tape_colour: tape_colour}
+      {chain: '10-speed',tire_size: '2.1',front_shock: front_shock,
+        rear_shock: rear_shock}
   end
 
 end
--- spec/inheritance_spec.rb
+++ spec/inheritance_spec.rb
@@ -58,7 +58,7 @@ describe RoadBike do
         expect(@bicycle.size).to eq 'M'
       end
 
-      it 'initializes with a size which can be read as an attribute' do
+      it 'initializes with a tape colour which can be read as an attribute' do
         expect(@bicycle.tape_colour).to eq 'yellow'
       end
     end
@@ -75,36 +75,43 @@ describe RoadBike do
     end
   end
 end
-#
-# describe RoadBike do
-#   describe '#initialize/new' do
-#     context 'it initializes with a size and tape_colour' do
-#       shared_context 'road_bike initialize' do
-#         before(:all) do
-#           @bicycle = described_class.new(size: 'M', tape_colour: 'yellow')
-#         end
-#       end
-#
-#       include_context 'road_bike initialize'
-#
-#       it 'initializes with a size which can be read as an attribute' do
-#         expect(@bicycle.size).to eq 'M'
-#       end
-#
-#       it 'initializes with a size which can be read as an attribute' do
-#         expect(@bicycle.tape_colour).to eq 'yellow'
-#       end
-#     end
-#   end
-#
-#   describe '#spare' do
-#     before(:each) do
-#       @road_bike = described_class.new(size: 'M',tape_colour: 'yellow')
-#     end
-#
-#     it 'returns a hash of required spare parts for a road bike' do
-#       expected_spares = {chain: '10-speed',tire_size: '23',tape_colour: 'yellow'}
-#       expect(@road_bike.spares).to eq expected_spares
-#     end
-#   end
-# end
+
+describe MountainBike do
+  describe '#initialize/new' do
+    context 'it initializes with a size and shocks' do
+      shared_context 'mountain_bike initialize' do
+        before(:all) do
+          @mountain_bike = described_class.new(size: 'M', front_shock: 'Shimano',
+          rear_shock: 'Continental')
+        end
+      end
+
+      include_context 'mountain_bike initialize'
+
+      it 'initializes with a size which can be read as an attribute' do
+        expect(@mountain_bike.size).to eq 'M'
+      end
+
+      it 'initializes with a front_shock which can be read as an attribute' do
+        expect(@mountain_bike.front_shock).to eq 'Shimano'
+      end
+
+      it 'initializes with a rear_shock which can be read as an attribute' do
+        expect(@mountain_bike.rear_shock).to eq 'Continental'
+      end
+    end
+  end
+
+  describe '#spare' do
+    before(:each) do
+      @mountain_bike = described_class.new(size: 'M', front_shock: 'Shimano',
+      rear_shock: 'Continental')
+    end
+
+    it 'returns a hash of required spare parts for a mountain bike' do
+      expected_spares = {chain: '10-speed',tire_size: '2.1',front_shock: 'Shimano',
+      rear_shock: 'Continental'}
+      expect(@mountain_bike.spares).to eq expected_spares
+    end
+  end
+end
