bdcca8566228d03ba22a5a46034ad21c580e19e3
Joe Zhou <chn.challenger@gmail.com>
2016-05-06 16:47:49 +0100

    can now cater for roadbike and mountain bike

--- models/inheritance.rb
+++ models/inheritance.rb
@@ -1,14 +1,23 @@
 class Bicycle
 
-  attr_reader :size, :tape_colour
+  attr_reader :size, :tape_colour, :type, :front_shock, :rear_shock
 
   def initialize(args)
     @size = args[:size]
     @tape_colour = args[:tape_colour]
+    @type = args[:type]
+    @front_shock = args[:front_shock]
+    @rear_shock = args[:rear_shock]
   end
 
   def spares
-    {chain: '10-speed',tire_size: '23',tape_colour: tape_colour}
+    case type
+    when 'roadbike'
+      {chain: '10-speed',tire_size: '23',tape_colour: tape_colour}
+    when 'mountainbike'
+      {chain: '10-speed',tire_size: '2.1',front_shock: front_shock,
+        rear_shock: rear_shock}
+    end
   end
 
 end
--- spec/inheritance_spec.rb
+++ spec/inheritance_spec.rb
@@ -5,7 +5,8 @@ describe Bicycle do
     context 'it initializes with a size and tape colour' do
       shared_context 'bicycle initialize' do
         before(:all) do
-          @bicycle = described_class.new(size: 'M', tape_colour: 'yellow')
+          @bicycle = described_class.new(size: 'M', tape_colour: 'yellow',
+            type: 'roadbike')
         end
       end
 
@@ -23,13 +24,21 @@ describe Bicycle do
 
   describe '#spare' do
     before(:each) do
-      @bicycle = described_class.new(size: 'M', tape_colour: 'yellow')
+      @road_bike = described_class.new(size: 'M',tape_colour: 'yellow',
+        type: 'roadbike')
+      @mountain_bike = described_class.new(size: 'L',front_shock: 'Shimano',
+        rear_shock:'Continental', type: 'mountainbike')
     end
 
-    it 'returns a hash of required spare parts' do
+    it 'returns a hash of required spare parts for a road bike' do
       expected_spares = {chain: '10-speed',tire_size: '23',tape_colour: 'yellow'}
-      expect(@bicycle.spares).to eq expected_spares
+      expect(@road_bike.spares).to eq expected_spares
     end
 
+    it 'returns a hash of required spare parts for a mountain bike' do
+      expected_spares = {chain: '10-speed',tire_size: '2.1',front_shock: 'Shimano',
+        rear_shock:'Continental'}
+      expect(@mountain_bike.spares).to eq expected_spares
+    end
   end
 end
