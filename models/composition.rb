3b25123c5c5d69d234ef010cb140d361525ff87c
Joe Zhou <chn.challenger@gmail.com>
2016-05-07 20:34:05 +0100

    added spare and == in order to test spare

--- models/composition.rb
+++ models/composition.rb
@@ -16,15 +16,24 @@ class Parts
 
   attr_reader :parts
 
-  def initialize(*part)
-    @parts = *part
+  def initialize(parts)
+    @parts = parts
   end
 
-  # def spare
-  #   spare_parts = []
-  #   parts.each do |part|
-  #
-  #   end
-  # end
+  def ==(parts_obj)
+    return false if self.parts.length != parts_obj.parts.length
+    for i in 0..self.parts.length - 1
+      return false if self.parts[i] != parts_obj.parts[i]
+    end
+    return true
+  end
+
+  def spare
+    spare_parts = []
+    parts.each do |part|
+      spare_parts << part if part.need_spare?
+    end
+    self.class.new(spare_parts)
+  end
 
 end
