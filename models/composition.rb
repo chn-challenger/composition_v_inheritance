e62616de5fb9dd124504945f82cd414770e8400f
Joe Zhou <chn.challenger@gmail.com>
2016-05-07 20:48:28 +0100

    parts made of individual part objects

--- models/composition.rb
+++ models/composition.rb
@@ -1,5 +1,4 @@
 class Bicycle2
-
   attr_reader :size, :parts
 
   def initialize(args={})
@@ -10,74 +9,26 @@ class Bicycle2
   def spares
     parts.spares
   end
-
 end
 
 class Parts
+  attr_reader :parts
 
-  attr_reader :chain, :tire_size
-
-  def initialize(args={})
-    @chain = args[:chain] || _default_chain
-    @tire_size = args[:tire_size] || _default_tire_size
-    _additional_init(args)
-  end
-
-  def _additional_init(args)
-  end
-
-  def _default_tire_size
-    raise 'Subclass must implement'
-  end
-
-  def _default_chain
-    '10-speed'
+  def initialize(parts)
+    @parts = parts
   end
 
   def spares
-      {tire_size:tire_size,chain:chain}.
-        merge(_special_spares)
+    parts.select {|part| part.need_spare}
   end
-
-  def _special_spares
-    {}
-  end
-
 end
 
-class RoadBikeParts < Parts
+class Part
+  attr_reader :name, :description, :need_spare
 
-  attr_reader :tape_colour
-
-  def _additional_init(args)
-    @tape_colour = args[:tape_colour]
+  def initialize(args)
+    @name = args[:name]
+    @description = args[:description]
+    @needs_spare = args.fetch(:needs_spare,true)
   end
-
-  def _default_tire_size
-    '23'
-  end
-
-  def _special_spares
-    {tape_colour:tape_colour}
-  end
-
-end
-
-class MountainBikeParts < Parts
-
-  attr_reader :front_shock, :rear_shock
-
-  def _additional_init(args)
-    @front_shock = args[:front_shock]
-    @rear_shock = args[:rear_shock]
-  end
-
-  def _default_tire_size
-    '2.1'
-  end
-
-  def _special_spares
-    {front_shock:front_shock,rear_shock:rear_shock}
-  end
-
 end
