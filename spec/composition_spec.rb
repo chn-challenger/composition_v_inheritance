265ff59d4157c29d67d2f7e6041365bd87f0bcfb
Joe Zhou <chn.challenger@gmail.com>
2016-05-09 11:24:05 +0100

    tests for bicycle and parts class

--- spec/composition_spec.rb
+++ spec/composition_spec.rb
@@ -26,60 +26,93 @@ describe Bicycle2 do
   end
 end
 
-# describe Parts do
-#   describe '#initialize/new' do
-#     it 'initialize with a collection of parts stored in parts attribute' do
-#       # part_1 = double(:part_1)
-#       # part_2 = double(:part_2)
-#       # parts_obj = described_class.new([part_1,part_2])
-#       # expect(parts_obj.parts).to eq [part_1,part_2]
-#       chain = Part.new(name:'chain',description:'12-speed')
-#       tire = Part.new(name:'tire',description:'21')
-#       shock = Part.new(name:'shock',description:'Shimano')
-#       # p shock
-#       mountain_bike = Bicycle2.new(size: 'L', parts: Parts.new([chain,tire,shock]))
-#       # mountain_bike.parts = [1,2,3]
-#       # p mountain_bike.partss
-#       # p mountain_bike.spares.size
-#       # p mountain_bike.parts.size
+describe Parts do
+  let(:part_1){double(:part_1)}
+  let(:part_2){double(:part_2)}
+  let(:parts){described_class.new([part_1,part_2])}
+
+  describe '#initialize/new' do
+    it 'initialize with an array of part objects' do
+      expect{described_class.new([double(:part_1),double(:part_2)])}.not_to raise_error
+    end
+  end
+
+  describe '#size' do
+    it 'returns the number of part objects contained in parts' do
+      expect(parts.size).to eq 2
+    end
+  end
+
+  describe '#each' do
+    it 'responds to method each' do
+      expect(parts).to respond_to(:each)
+    end
+  end
+
+  describe '#spare' do
+    it 'returns an array of spares' do
+      allow(part_1).to receive(:needs_spare).and_return(true)
+      allow(part_2).to receive(:needs_spare).and_return(false)
+      expect(parts.spares).to eq [part_1]
+    end
+  end
+
+  #
+  # describe '==' do
+  #   it 'returns true when all parts are equal' do
+  #     part_1 = double(:part_1)
+  #     part_2 = double(:part_2)
+  #     parts_1 = described_class.new([part_1,part_2])
+  #     parts_2 = described_class.new([part_1,part_2])
+  #     expect(parts_1).to eq parts_2
+  #   end
+  # end
+  #
+  # describe '#spare' do
+  #   it 'returns a parts object with a collect of needed spare parts' do
+  #     part_1 = double(:part_1,need_spare?:true,name:'chain',description:'10-speed')
+  #     part_2 = double(:part_2,need_spare?:true,name:'tire_size',description:'21')
+  #     part_3 = double(:part_3,need_spare?:false,name:'frame',description:'Specialized')
+  #     parts_obj = described_class.new([part_1,part_2,part_3])
+  #     spare_parts = described_class.new([part_1,part_2])
+  #     expect(parts_obj.spare).to eq spare_parts
+  #   end
+  # end
+
+end
+
+
+
+
+
+# # part_1 = double(:part_1)
+# # part_2 = double(:part_2)
+# # parts_obj = described_class.new([part_1,part_2])
+# # expect(parts_obj.parts).to eq [part_1,part_2]
+# chain = Part.new(name:'chain',description:'12-speed')
+# tire = Part.new(name:'tire',description:'21')
+# shock = Part.new(name:'shock',description:'Shimano')
+# # p shock
+# mountain_bike = Bicycle2.new(size: 'L', parts: Parts.new([chain,tire,shock]))
+# # mountain_bike.parts = [1,2,3]
+# # p mountain_bike.partss
+# # p mountain_bike.spares.size
+# # p mountain_bike.parts.size
 #
-#       road_config =
-#         [['chain',        '10-speed'],
-#          ['tire_size',    '23'],
-#          ['tape_colour',  'red']]
+# road_config =
+#   [['chain',        '10-speed'],
+#    ['tire_size',    '23'],
+#    ['tape_colour',  'red']]
 #
-#       mountain_config =
-#         [['chain',        '10-speed'],
-#          ['tire_size',    '2.1'],
-#          ['front_shock',  'Manitou', false],
-#          ['rear_shock',   'Fox']]
+# mountain_config =
+#   [['chain',        '10-speed'],
+#    ['tire_size',    '2.1'],
+#    ['front_shock',  'Manitou', false],
+#    ['rear_shock',   'Fox']]
 #
 #
-#       road_parts = PartsFactory.build(road_config)
-#       road_bike = Bicycle2.new(size:'L', parts: road_parts)
-#       p road_bike
-#     end
-#   end
-#   #
-#   # describe '==' do
-#   #   it 'returns true when all parts are equal' do
-#   #     part_1 = double(:part_1)
-#   #     part_2 = double(:part_2)
-#   #     parts_1 = described_class.new([part_1,part_2])
-#   #     parts_2 = described_class.new([part_1,part_2])
-#   #     expect(parts_1).to eq parts_2
-#   #   end
-#   # end
-#   #
-#   # describe '#spare' do
-#   #   it 'returns a parts object with a collect of needed spare parts' do
-#   #     part_1 = double(:part_1,need_spare?:true,name:'chain',description:'10-speed')
-#   #     part_2 = double(:part_2,need_spare?:true,name:'tire_size',description:'21')
-#   #     part_3 = double(:part_3,need_spare?:false,name:'frame',description:'Specialized')
-#   #     parts_obj = described_class.new([part_1,part_2,part_3])
-#   #     spare_parts = described_class.new([part_1,part_2])
-#   #     expect(parts_obj.spare).to eq spare_parts
-#   #   end
-#   # end
-#
+# road_parts = PartsFactory.build(road_config)
+# road_bike = Bicycle2.new(size:'L', parts: road_parts)
+# p road_bike
+# end
 # end
